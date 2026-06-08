ee4f67cd9934802b650bee3deff439fdcdf375d6
Fuzimoto Syouhei <B0024029@ib.yic.ac.jp>
2026-06-08 12:35:27 +0900

    備考欄の追加

--- ShiftManagerApi2/ShiftSubmission.cs
+++ ShiftManagerApi2/ShiftSubmission.cs
@@ -12,6 +12,8 @@ namespace ShiftManagerApi2
         // 👤 スタッフ名 (userName) を受け取る
         public string Name { get; set; } = string.Empty;
 
+        public string Memo { get; set; } = string.Empty;
+
         // 📅 選択された日付のリスト (sortedDates) を受け取る
         public List<ShiftDateItem> Dates { get; set; } = new List<ShiftDateItem>();
     }
