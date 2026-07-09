5d59ff5ea47743fe7968d9012a45d97089ccb808
SyouheiFuzimoto <B0024029@ib.yic.ac.jp>
2026-07-09 14:05:03 +0900

    Add files via upload

--- admin-page/app.js
+++ admin-page/app.js
@@ -53,7 +53,7 @@ function CalendarYearAndMonth() {
         })
 }
 function CalendarData(ymdata) {
-    fetch('https://overplay-patriarch-daffodil.ngrok-free.dev/api/admin/calendarnau', {
+    fetch(`https://overplay-patriarch-daffodil.ngrok-free.dev/api/admin/calendarnau?periods_id=${ymdata.id}`, {
         headers: {
             'ngrok-skip-browser-warning': 'true'
         }
