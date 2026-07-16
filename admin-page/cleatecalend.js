e95e1b202cfa8907758c26f0fa3de92e5c248f78
SyouheiFuzimoto <B0024029@ib.yic.ac.jp>
2026-07-16 12:27:15 +0900

    Add files via upload

--- admin-page/cleatecalend.js
+++ admin-page/cleatecalend.js
@@ -74,7 +74,7 @@ function GetEvent(Id) {//カレンダーのidからイベントを探す
         .then(response => {
             if (!response.ok)
                 console.log(response);
-                throw new Error('データの取得に失敗したよイベントの');
+            throw new Error(`データの取得に失敗したよイベントの${response.status} エラー:${resonse.statusText}`);
             return response.json();
         })
         .then(date => {
